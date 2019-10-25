610d697e01684193032ed4d54f8d051c17e6baaf
Amanda Murphy <amurphy@newrelic.com>
2019-10-25 10:35:27 -0700

    allow argument overriding when the daemon command is passed in

--- 1.2.0/docker-entrypoint.sh
+++ 1.2.0/docker-entrypoint.sh
@@ -9,9 +9,11 @@ defaultArgs=" --logfile /proc/self/fd/1 --watchdog-foreground --address=$(hostna
 
 # this if will check if the first argument is a flag
 if [ "${1:0:1}" = '-' ]; then
-  set -- /usr/bin/newrelic-daemon "$@" "$defaultArgs"
+  set -- /usr/bin/newrelic-daemon $defaultArgs "$@"
 elif [ "$1" = '/usr/bin/newrelic-daemon' ]; then
-  set -- "$@" "$defaultArgs"
+  # Remove the first element from the arguments
+  shift 1
+  set -- /usr/bin/newrelic-daemon $defaultArgs "$@"
 fi
 
 exec "$@"
