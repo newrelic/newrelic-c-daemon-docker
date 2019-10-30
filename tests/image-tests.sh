155ce748883d8e622688604a793b70fc29efc964
Amanda Murphy <amurphy@newrelic.com>
2019-10-30 10:26:28 -0700

    config file

--- tests/image-tests.sh
+++ tests/image-tests.sh
@@ -23,11 +23,11 @@ run_tests() {
 
   name=debug-daemon
   docker run -d --name $name c-daemon --loglevel debug
-  verify_output "-n $(docker logs $name | grep debug)" $name "set the loglevel to debug"
+  verify_output "-n $(docker logs $name | grep Debug)" $name "set the loglevel to debug"
 
   name=debug-daemon-with-cmd
   docker run -d --name $name c-daemon --loglevel debug
-  verify_output "-n $(docker logs $name | grep debug)" $name "set the loglevel to debug using cmd override"
+  verify_output "-n $(docker logs $name | grep Debug)" $name "set the loglevel to debug using cmd override"
 
   name=no-logs
   docker run -d --name $name c-daemon --logfile /var/log/newrelic/newrelic.log
@@ -36,6 +36,12 @@ run_tests() {
   name=no-logs-with-cmd
   docker run -d --name $name c-daemon /usr/bin/newrelic-daemon --logfile /var/log/newrelic/newrelic.log
   verify_output "-n $(docker exec $name sh -c 'exec cat /var/log/newrelic/newrelic.log')" $name "set the logfile to /var/log/newrelic/newrelic.log using cmd override"
+
+  name=cfg-debug
+  echo "loglevel=debug" > newrelic.cfg
+  docker run -d --name $name -v $PWD/newrelic.cfg:/etc/newrelic/newrelic.cfg c-daemon --c /etc/newrelic/newrelic.cfg
+  verify_output "-n $(docker logs $name | grep Debug)" $name "set the loglevel to debug using cfg file"
+
 }
 
 versions=(*/)
