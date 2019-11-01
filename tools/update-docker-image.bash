6a1d3393437b513fd4cdd17623e8ef1bed29a7d6
Amanda Murphy <amurphy@newrelic.com>
2019-11-01 16:02:29 -0700

    add automation

---
+++ tools/update-docker-image.bash
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -ex
+
+if [[ ! "$1" =~ [0-9]+\.[0-9]+\.[0-9]+ ]]; then
+  echo "ERROR: A version string must be passed in"
+  exit 1
+fi
+
+version=$1
+version_short=$(echo $1 | awk -F "." '{print $1 "." $2 "." $3}')
+
+mkdir $version_short
+
+cp docker-entrypoint-template $version_short/docker-entrypoint.sh
+cd $version_short
+
+sed s/"ENV[[:space:]]NEWRELIC_VERSION/ENV NEWRELIC_VERSION v${version}"/ ../Dockerfile-template > Dockerfile
+
+# git checkout -b $version_short
+# git add .
+# git commit -m "version bump to ${version_short}"
